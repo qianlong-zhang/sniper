39f1f8bb1dd7ac53b57410e09120d59bd06d862b
Qianlong Zhang <qianlong2006@gmail.com>
2018-01-16 09:16:48 -0500

    add Olden benchmark

--- gdb-run-sniper.sh
+++ gdb-run-sniper.sh
@@ -1,7 +1,7 @@
 #./run-sniper --gdb-wait ./HPTW/hptw
-#./run-sniper -c mydunnington --gdb-wait  ./HPTW/hptw
-#./run-sniper -c mydunnington -n 1 --traces=./HPTW/Hptw_test/hptw.sift 2>./HPTW/Hptw_test/hptw-core.log
+#./run-sniper -c my --gdb-wait  ./HPTW/hptw
+#./run-sniper -c my -n 1 --traces=./HPTW/Hptw_test/hptw.sift 2>./HPTW/Hptw_test/hptw-core.log
 #./run-sniper -c gainestown -n 1 --traces=./HPTW/Mcf/mcf.sift 2>./HPTW/Mcf/mcf-core.log
-#./run-sniper -c mydunnington-linked-prefetch -n 1 --traces=./link.sift 2>link.log
-#./run-sniper -c mydunnington-noprefetch -n 1 --traces=./link.sift 2>link.log
-./run-sniper -c mydunnington-simple-prefetch -n 1 --traces=./lat_mem_rd.sift 2>lat_mem_rd.log
+#./run-sniper -c my-linked-prefetch -n 1 --traces=./link.sift 2>link.log
+#./run-sniper -c my-noprefetch -n 1 --traces=./link.sift 2>link.log
+./run-sniper -c my-simple-prefetch -n 1 --traces=./lat_mem_rd.sift 2>lat_mem_rd.log
